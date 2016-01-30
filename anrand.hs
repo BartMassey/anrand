2f0efac7dcd0c72a39744f5022ff8ddba4c64327
Bart Massey <bart@cs.pdx.edu>
2016-01-30 14:29:06 -0800

    did cleanups; added entropy calculation

--- anrand.hs
+++ anrand.hs
@@ -4,16 +4,15 @@ import Data.Colour.Names
 import Data.Default.Class
 import Graphics.Rendering.Chart
 import Graphics.Rendering.Chart.Backend.Cairo
-import Graphics.Rendering.Chart.Layout
-import Graphics.Rendering.Chart.Plot.Histogram
-import Graphics.Rendering.Chart.Plot.Lines
-import Graphics.Rendering.Chart.Plot.Points
 
 import Data.Bits
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import Data.List
 import Text.Printf
+
+analysis :: String -> String -> String
+analysis what suff = "analysis/" ++ what ++ suff
 
 pdfRender :: String -> Renderable a -> IO ()
 pdfRender name r = do
@@ -32,15 +31,20 @@ readSamples stuff =
           in
           ((i1 `shiftL` 8) .|. i2) : makeInts bs
             
-textHist :: [Int] -> String
-textHist samples =
-    unlines $ map histify $
-     group $ sort $ samples ++ [smallest..largest]
+rawHist :: [Int] -> [(Int, Int)]
+rawHist samples =
+    map histify $ group $ sort $ samples ++ [smallest..largest]
     where
       smallest = minimum samples
       largest = maximum samples
       histify [] = error "bad hist group"
-      histify (x : xs) = unwords [show x, show (length xs)]
+      histify (x : xs) = (x, length xs)
+
+showHist :: [(Int, Int)] -> String
+showHist bins =
+    unlines $ map showBin bins
+    where
+      showBin (x, y) = unwords [show x, show y]
 
 plotTimeSeries :: [Int] -> Renderable (LayoutPick Int Int Int)
 plotTimeSeries samples = do
@@ -80,30 +84,60 @@ plotSampleHist samples = do
           def
   layoutToRenderable histLayout
 
+entropy :: [(Int, Int)] -> Double
+entropy samples =
+    negate $ sum $ map binEntropy samples
+    where
+      nStates = sum $ map snd samples
+      binEntropy (_, 0) = 0
+      binEntropy (_, count) =
+          p * logBase 2 p
+          where
+            p = fromIntegral count / fromIntegral nStates
+
+-- The specific data we're looking at has a mod 4 entropy problem.
+-- We'll deal with it because we need to.
+mod4entropy :: [Int] -> Double
+mod4entropy samples =
+    entropy mod4sums
+    where
+      mod4sums = sumMod 4 $ rawHist samples
+      sumMod :: Int -> [(Int, Int)] -> [(Int, Int)]
+      sumMod m [] = zip [0..m - 1] $ replicate m 0
+      sumMod m ((x, y) : xs) =
+          map (addBin (x `mod` m, y)) $ sumMod m xs
+          where
+            addBin :: (Int, Int) -> (Int, Int) -> (Int, Int)
+            addBin (i, j) (i', j') | i == i' = (i, j + j')
+            addBin (_, _) (i', j') = (i', j')
+                
+
 showStats :: [Int] -> String
 showStats samples =
   let nSamples = length samples in
-  printf "min: %d  max: %d  mean: %g\n"
+  printf "min: %d  max: %d  mean: %0.3g\nentropy: %0.3g  mod4entropy: %0.3g\n"
       (minimum samples)
       (maximum samples)
       (fromIntegral (sum samples) / fromIntegral nSamples :: Double)
+      (entropy $ rawHist samples)
+      (mod4entropy samples)
 
-plotSamples :: String -> [Int] -> IO ()
-plotSamples what samples = do
-  writeFile (what ++ "-stats.txt") $ showStats samples
-  pdfRender (what ++ "-ts.pdf") $ plotTimeSeries samples
-  pdfRender (what ++ "-hist.pdf") $ plotSampleHist samples
-  writeFile (what ++ "-hist.txt") $ textHist samples
+analyze :: String -> [Int] -> IO ()
+analyze what samples = do
+  writeFile (analysis what "-stats.txt") $ showStats samples
+  pdfRender (analysis what "-ts.pdf") $ plotTimeSeries samples
+  pdfRender (analysis what "-hist.pdf") $ plotSampleHist samples
+  writeFile (analysis what "-hist.txt") $ showHist $ rawHist samples
 
 main :: IO ()
 main = do
   rawSamples <- B.getContents
   let samples = readSamples rawSamples
 
-  plotSamples "raw" samples
+  analyze "raw" samples
 
   let lowSamples = map (.&. 0xff) samples
-  plotSamples "low" lowSamples
+  analyze "low" lowSamples
 
   let midSamples = map ((.&. 0xff) . (`shiftR` 1)) samples
-  plotSamples "mid" midSamples
+  analyze "mid" midSamples
