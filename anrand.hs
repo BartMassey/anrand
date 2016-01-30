f5d5e4c87110a12395a477b3a40a94c91cd1813c
Bart Massey <bart@cs.pdx.edu>
2016-01-29 22:45:05 -0800

    got first cut of working analysis graphs

--- anrand.hs
+++ anrand.hs
@@ -1,10 +1,24 @@
-import qualified Data.Vector as V
-import Graphics.Rendering.Plot
-import Numeric.FFT.Vector.Unnormalized
+import Control.Lens
+import Data.Colour
+import Data.Colour.Names
+import Data.Default.Class
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Backend.Cairo
+import Graphics.Rendering.Chart.Layout
+import Graphics.Rendering.Chart.Plot.Histogram
+import Graphics.Rendering.Chart.Plot.Lines
+import Graphics.Rendering.Chart.Plot.Points
+
 import Data.Bits
-import Data.List
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+import Data.List
+import Text.Printf
+
+pdfRender :: String -> Renderable a -> IO ()
+pdfRender name r = do
+    renderableToFile (fo_format .~ PDF $ def) name r
+    return ()
 
 readSamples :: B.ByteString -> [Int]
 readSamples stuff =
@@ -18,14 +32,78 @@ readSamples stuff =
           in
           ((i1 `shiftL` 8) .|. i2) : makeInts bs
             
-textHist :: B.ByteString -> B.ByteString
-textHist stuff =
-    BC.pack $ unlines $ map printbin $ group $
-    sort $ (++ [0..(4095 :: Int)]) $ readSamples stuff
+textHist :: [Int] -> String
+textHist samples =
+    unlines $ map histify $
+     group $ sort $ samples ++ [smallest..largest]
     where
-      printbin [] = error "missing value"
-      printbin (x : xs) =
-          unwords [show x, show $ length xs]
+      smallest = minimum samples
+      largest = maximum samples
+      histify [] = error "bad hist group"
+      histify (x : xs) = unwords [show x, show (length xs)]
+
+plotTimeSeries :: [Int] -> Renderable (LayoutPick Int Int Int)
+plotTimeSeries samples = do
+  let nSamples = length samples
+  let samplePoints = zip [(1::Int)..] samples
+  let zoomedPoints =
+          zip [0, nSamples `div` 100 ..] $
+              take 100 $ drop (nSamples `div` 3) samples
+  let allPlot =
+          toPlot $
+          plot_points_style .~ filledCircles 0.5 (opaque green) $
+          plot_points_values .~ samplePoints $
+          plot_points_title .~ "all" $
+          def
+  let zoomedPlot =
+          toPlot $
+          plot_lines_style . line_color .~ opaque red $
+          plot_lines_values .~ [zoomedPoints] $
+          plot_lines_title .~ "zoomed" $
+          def
+  let tsLayout =
+          layout_title .~ "Time Series" $
+          layout_plots .~ [allPlot, zoomedPlot] $
+          def
+  layoutToRenderable tsLayout
+
+plotSampleHist :: [Int] -> Renderable (LayoutPick Double Int Int)
+plotSampleHist samples = do
+  let histPlot =
+          histToPlot $
+          plot_hist_title .~ "Sample Histogram" $
+          plot_hist_bins .~ maximum samples - minimum samples + 1 $
+          plot_hist_values .~  map fromIntegral samples $
+          (defaultPlotHist :: PlotHist Double Int)
+  let histLayout =
+          layout_plots .~ [histPlot] $
+          def
+  layoutToRenderable histLayout
+
+showStats :: [Int] -> String
+showStats samples =
+  let nSamples = length samples in
+  printf "min: %d  max: %d  mean: %g\n"
+      (minimum samples)
+      (maximum samples)
+      (fromIntegral (sum samples) / fromIntegral nSamples :: Double)
+
+plotSamples :: String -> [Int] -> IO ()
+plotSamples what samples = do
+  writeFile (what ++ "-stats.txt") $ showStats samples
+  pdfRender (what ++ "-ts.pdf") $ plotTimeSeries samples
+  pdfRender (what ++ "-hist.pdf") $ plotSampleHist samples
+  writeFile (what ++ "-hist.txt") $ textHist samples
 
 main :: IO ()
-main = B.interact textHist
+main = do
+  rawSamples <- B.getContents
+  let samples = readSamples rawSamples
+
+  plotSamples "raw" samples
+
+  let lowSamples = map (.&. 0xff) samples
+  plotSamples "low" lowSamples
+
+  let midSamples = map ((.&. 0xff) . (`shiftR` 1)) samples
+  plotSamples "mid" midSamples
